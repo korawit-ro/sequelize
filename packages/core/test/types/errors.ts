d941d2696d56bcf860f43058c40515682f5d7672
Korawit Romsuk <korawit.ro@outlook.com>
2023-10-24 21:57:01 +0700

    #ci: clean checkpoint

--- packages/core/test/types/errors.ts
+++
@@ -1,7 +0,0 @@
-import { expectTypeOf } from 'expect-type';
-import type { BaseError, EmptyResultError, OptimisticLockError, UniqueConstraintError } from '@sequelize/core';
-
-expectTypeOf<UniqueConstraintError>().toHaveProperty('sql').toBeString();
-expectTypeOf<EmptyResultError>().toMatchTypeOf<BaseError>();
-expectTypeOf<UniqueConstraintError>().toMatchTypeOf<BaseError>();
-expectTypeOf<OptimisticLockError>().toMatchTypeOf<BaseError>();
